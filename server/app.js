f1dc63dd047ef7c9a585d3bdfd3be7e3c1afc4e8
sirwan <sir1afifi@gmail.com>
2020-01-01 14:11:16 +0330

    add error handling feature

--- server/app.js
+++ server/app.js
@@ -1,15 +1,35 @@
 import express from "express";
 import morgan from "morgan";
+// Routings
+import indexRouter from "./routes/index";
+import { config } from "./config";
 
 const app = express();
 
-// Routings
-import indexRouter from "./routes/index";
-
 // Middlewares
-app.use(morgan("combined"));
+
+if (config.env === "development") {
+  app.use(morgan("combined"));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl}`,
+  });
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 export { app };
