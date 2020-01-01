e7b8c170bcc877580eaf9014335405d6ce870946
shaho <toofani@gmail.com>
2020-01-01 20:01:14 +0330

    switch to yarn, add color package, add license

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 import * as dotenv from "dotenv";
+import colors from "colors"
+
 import { app } from "./app";
 import { config } from "./config";
 dotenv.config();
 
 app.listen(config.port, () => {
-  console.log(`✅ App running on port ${config.port}`);
+  console.log(
+    `🚀  Server running in ${config.env} mode on port ${config.port}`.yellow
+      .bold,
+  )
+});
+
+// Handle unhandled promise rejection
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Error ${error.message}`.red);
+  // Close server and exit proccess
+  server.close(() => process.exit(1));
 });
