af23482fb38bccc748ea992138c1245b78b6e17b
shaho <toofani@gmail.com>
2020-01-05 12:23:54 +0330

    add models, dummy data

---
+++ server/controllers/links.js
@@ -0,0 +1,9 @@
+import Link from "../models/Link";
+
+// ─── GET LINKS ──────────────────────────────────────────────────────────────────
+// @desc    Get all links
+// @route   GET /api/v1/links
+// @access  Private
+export const getLinks = async () => {
+  //
+};
